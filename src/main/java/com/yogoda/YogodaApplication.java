099aba9ed175978d9dd8acac71717215828ab0e9
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-11 19:15:41 +0900

    📚BUILD : 스프링부트 3.2.8 기반 빌드

---
+++ src/main/java/com/yogoda/YogodaApplication.java
@@ -0,0 +1,13 @@
+package com.yogoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogodaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogodaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yogoda/YogodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogodaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
