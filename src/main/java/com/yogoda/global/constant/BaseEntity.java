a9efbfea25261fe630749533ea0e6c632212a4b6
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-11 19:43:41 +0900

    ✨FEAT : 갱신 및 수정 정보 날짜 고정 Entity 추가

---
+++ src/main/java/com/yogoda/global/constant/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.yogoda.global.constant;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
