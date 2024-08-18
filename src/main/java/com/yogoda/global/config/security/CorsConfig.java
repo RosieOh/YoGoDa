ff3bb6dd6cce47005e57a7c9761caec810394a22
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-18 23:44:38 +0900

    FEAT | #4 | Cors Error 방지 설정 로직 구현

---
+++ src/main/java/com/yogoda/global/config/security/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.yogoda.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", config);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
